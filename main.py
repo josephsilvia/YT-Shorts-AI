9ed9151c94543df290a2793eb4d608cece690d57
josephsilvia <marshallsilvia@gmail.com>
2023-02-25 22:39:48 -0800

    Test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from twisted.python.util import println
+
 
 import Variables
 
@@ -9,15 +7,15 @@ from Cooking_Facts_Generator import cooking_facts_generator
 
 
 def main():
- #  login_success = login(Variables.username, Variables.password)
+   login_success = login(Variables.username, Variables.password)
 
-  #  if login_success:
-    #     println("Youtube login was successful")
-   # else:
-   #     println("Youtube login was not successful")
+   if login_success:
+        print("YouTube login was successful")
+   else:
+       print("YouTube login was not successful")
 
 
-    cooking_facts_generator()
+
 
 
 if __name__ == '__main__':
